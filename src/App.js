b5b9f87c8cf2dfe7b337933bf2ced88cb4dc6b90
Luis Miguel Cupitra Vergara <luismiguelcupitravergara@Luiss-MacBook-Air.local>
2021-09-10 10:23:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import background from './assests/img/gettyimages-1184156438-2048x2048.jpg'
+import isoTipo from './assests/img/logos greenatics-iso.png'
+import underConstruction from './assests/img/underConstruction.png'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main" style={{backgroundImage:`url(${background})`}}>
+      <img className='logo-whitemark' src={isoTipo}></img>
+      <div className='content-text'>
+        <img  src={underConstruction}></img>
+      </div>
     </div>
   );
 }
